583b829db25370b165371f2795395fbae6dabd5f
Aldrei Glenn Nuqui <68434908+SuichiNagi@users.noreply.github.com>
2021-03-05 01:40:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-function keySound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+const animate = (key) => {
+    const currentKey = document.querySelector(`.${key}`)
+    currentKey.classList.add('pressed')
+    setTimeout(() => {
+        currentKey.classList.remove('pressed')   
+    }, 150)
+}
+
+const playMusic = (path) => {
+    const audio = new Audio(path);
     audio.play();
-    key.classList.add('playing');
 }
 
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing')
+document.addEventListener('keypress', (e) => {
+    const triggeredKey = e.key;
+    soundKey(triggeredKey);
+    animate(triggeredKey);
+})
+
+const soundKey = (key) => {
+    switch (key) {
+        case "a":
+        playMusic('kit-sounds/Kick.wav');
+        break;
+        case "s":
+        playMusic('kit-sounds/Snare.wav');
+        break;
+        case "d":
+        playMusic('kit-sounds/Tom 8in.wav');
+        break;
+        case "f":
+        playMusic('kit-sounds/Tom 10in.wav');
+        break;
+        case "t":
+        playMusic('kit-sounds/Crash.wav');
+        break;
+        case "y":
+        playMusic('kit-sounds/Trash.wav');
+        break;
+        case "g":
+        playMusic('kit-sounds/Floor Tom.wav');
+        break;
+        case "j":
+        playMusic('kit-sounds/Hi Hat Acoustic Closed.wav');
+        break;
+        case "k":
+        playMusic('kit-sounds/Hi Hat Acoustic Open.wav');
+        break;
+        case "l":
+        playMusic('kit-sounds/Ride Cymbal.wav');
+        break;
+    }
 }
 
-const key = document.querySelectorAll('.key');
-key.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', keySound);
+function clickKeyDrum(e){
+    var innerHTML = e.target.innerHTML; 
+    animate(innerHTML)
+    soundKey(innerHTML)
+}
 
+var keyDrum = document.querySelectorAll('.drum')
+for(let i = 0; i < keyDrum.length; i++){
+    keyDrum[i].addEventListener('click', clickKeyDrum);
+};
